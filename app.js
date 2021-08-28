ccba4a7b80829961a378703e884ecf1ef64bc4fa
fjcharlot <fccharlo@amazon.com>
2021-08-28 17:05:24 -0500

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello FRED, I am HAL!'));
+app.get('/', (req, res) => res.send('Hello FRED:: I am HAL...'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
